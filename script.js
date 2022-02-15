844b6593637b8bd84f5f425c7e7139224a4af3b5
KariinDeLeeuw <65500409+KariinDeLeeuw@users.noreply.github.com>
2022-02-15 12:54:21 +0100

    added js page and instructions

---
+++ script.js
@@ -0,0 +1,4 @@
+// - Receive an error message when the `form` is submitted if:
+//   - Any `input` field is empty. The message for this error should say _"[Field Name] cannot be empty"_
+//   - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say _"Looks like this is not an email"_
+
