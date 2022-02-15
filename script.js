4c351f57a5d87efb030a3e7488c74ccf943eb965
KariinDeLeeuw <65500409+KariinDeLeeuw@users.noreply.github.com>
2022-02-15 13:05:31 +0100

    added id's and got button out of html

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 //   - Any `input` field is empty. The message for this error should say _"[Field Name] cannot be empty"_
 //   - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say _"Looks like this is not an email"_
 
+// Got button out of HTML document
+const button = document.getElementById("btn");
+// Added an eventlistener and a function
+button.addEventListener("click", myFunction);
+
+function myFunction() {
+  alert("help");
+}
